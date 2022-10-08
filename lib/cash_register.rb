f0b3521a678b2faecf051aa547151dcabccb177d
Bobby Hinton <bob.g.hint@gmail.com>
2022-10-08 17:37:05 -0500

    commit tree 079a5c5d150d3b6464b7a49e0aabe9c4856e496d

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,43 @@
+class CashRegister
+  attr_reader :discount
+  attr_writer :total
+  attr_accessor :items, :last_transaction_total
 
+  def initialize(discount = 0)
+    @discount = discount
+    @total = 0
+    @items = []
+  end
+
+  # getter method for @total
+  def total
+    @total
+  end
+
+  def add_item(title, price, quantity = 1)
+     if quantity > 1
+      @total += price * quantity
+      quantity.times { |i| @items << title }
+      @last_transaction_total = price * quantity
+     else
+      @total += price
+      @items << title
+      @last_transaction_total = price
+     end
+  end
+
+  def apply_discount
+    if discount != 0
+      percent_disc = @discount.to_f/100.0
+      @total = @total - (percent_disc * @total)
+      "After the discount, the total comes to $#{self.total.to_i}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+  def void_last_transaction
+    self.items.pop()
+    @total = @total - @last_transaction_total
+  end
+end
